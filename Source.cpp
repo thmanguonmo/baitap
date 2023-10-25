539771fe0cf1eb9d1dd883925c2a580d8957764a
Your Name <phams2608@gmail.com>
2023-10-25 10:52:07 +0700

    tich

--- Source.cpp
+++ Source.cpp
@@ -2,17 +2,25 @@
 
 using namespace std;
 // Tran Van Si
+
 int tong(int a, int b){
 	return a+b;
 }
 int hieu(int a, int b);
-int tich(int a, int b);
+//Pham Huu Truong Son
+int tich(int a, int b){
+	return a*b;
+}
 float thuong(int a, int b)
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	// Tong
 	cout<<tong(2,3);
+	//tich
+	cout<< tich(3,4);
+	
 	system("pause");
+	
 	return 0;
 }
