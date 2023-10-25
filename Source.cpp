3157be3f2d1034cccd0a763e784496c63138232a
Dinhduy789 <0982093650a@>
2023-10-25 10:55:26 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -8,11 +8,18 @@ int tong(int a, int b){
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b)
+{
+if (b!=0)
+return a/b;
+else
+return 0;}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	// Tong
 	cout<<tong(2,3);
+	// thuong
+	cout<<thuong(2,4);
 	system("pause");
 	return 0;
 }
