4f3bb438f8ee42093fafa159a18b60cae8e5f6b3
Ky <ngocky26072002@gmail.com>
2023-10-25 10:43:52 +0700

    Hieu

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){//ky
+	return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	//tinh hieu
+	cout<<"Hieu cua a-b="<<hieu<<endl;
+	
 	system("pause");
 	return 0;
 }
