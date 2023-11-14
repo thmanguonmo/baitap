453c5f486d19d55bcad7885d38c595fe9be0b0ed
NgocKy <ngocky26072002@gmail.com>
2023-11-14 19:00:27 +0700

    tinh tich va thuong

--- Source.cpp
+++ Source.cpp
@@ -11,17 +11,12 @@ int hieu(int a, int b){//ky
 
 // Tran Van Si
 
-int tong(int a, int b){
-	return a+b;
+int tich(int a, int b) {
+	return a * b;
 }
-
-float thuong(int a, int b)
-{
-if (b!=0)
-return a/b;
-else
-return 0;}
-=======
+int thuong(int a, int b) {
+	return a / b;
+}
 //Pham Huu Truong Son
 int tich(int a, int b){
 	return a*b;
@@ -33,9 +28,8 @@ int main()
 	// Tong
 	cout<<tong(2,3);
 	// thuong
-	cout<<thuong(2,4);
-	//tich
-	cout<< tich(3,4);
+	cout << "Tich: " << tich(3, 4) << endl;
+	cout << "Thuong: " << thuong(4, 2) << endl;
 
 	
 	system("pause");
